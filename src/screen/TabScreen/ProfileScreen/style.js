052deafe7cd49847bb5c3878bb66b31da485cf5d
dtthong31 <hongblack2000@gmail.com>
2022-02-27 12:33:40 +0700

    UI ProfileScreen complete

---
+++ src/screen/TabScreen/ProfileScreen/style.js
@@ -0,0 +1,60 @@
+import { COLORS } from "../../../themes/color";
+
+const { StyleSheet } = require("react-native");
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    contentView: {
+        flex: 2,
+        padding: 16,
+        paddingTop: 30,
+        backgroundColor: '#fff',
+        // alignItems: 'center'
+    },
+    headerView: {
+        flex: 0.8,
+        alignItems: 'center',
+        backgroundColor: '#4baef9'
+    },
+    imgView: {
+        backgroundColor: COLORS.lightPink,
+        width: 110,
+        height: 110,
+        borderRadius: 55,
+        borderWidth: 2,
+        borderColor: 'white'
+    },
+    textHeader: {
+        fontSize: 25,
+        color: "white"
+    },
+    textSVHeader: {
+        fontSize: 22,
+        color: "white", paddingTop: 5
+    },
+    head: { height: 40 },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, alignItems: 'flex-start' },
+    row: { height: 28 },
+    text: { textAlign: 'center', fontSize: 15 },
+    btnStyle: {
+        width: "90%",
+        height: 60,
+        backgroundColor: "#4baef9",
+        borderRadius: 10,
+        marginTop: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textBtn: {
+        fontSize: 30,
+        fontWeight: '600',
+        color: 'white'
+    },
+    btnView: {
+        alignItems: 'center'
+    }
+
+})
